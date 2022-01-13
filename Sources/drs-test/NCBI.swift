49cd551333d8bfddd7b9eb573b5321c84f2ba255
Kenneth Durbrow <kmdurbrow@me.com>
2022-01-13 14:08:39 -0500

    Added more parameters; refactoring

--- Sources/drs-test/NCBI.swift
+++ Sources/drs-test/NCBI.swift
@@ -24,8 +24,14 @@ struct SRAA {
             case response
         }
 
-        static func url(for accession: String) -> String {
-            "idx/v1/\(accession)"
+        static func url(for accession: String, submitted: Bool = true, ETL: Bool = false) -> String {
+            if submitted && !ETL {
+                return "idx/v1/\(accession)"
+            }
+            var query = [String]()
+            if !submitted { query.append("submitted=false") }
+            if ETL { query.append("etl=true")}
+            return "idx/v1/\(accession)?\(query.joined(separator: "&"))"
         }
     }
     enum SystemType {
@@ -40,38 +46,65 @@ struct SRAA {
 }
 
 extension SRAA {
-    private static var devURL = URL(string: "https://locate-dev.ncbi.nlm.nih.gov/")!
-    private static var prdURL = URL(string: "https://locate.ncbi.nlm.nih.gov/")!
-    private static var rgnURL = URL(string: "https://locate.be-md.ncbi.nlm.nih.gov/")!
+    private static let devURL = URL(string: "https://locate-dev.ncbi.nlm.nih.gov/")!
+    private static let prdURL = URL(string: "https://locate.ncbi.nlm.nih.gov/")!
+    private static let rgnURL = URL(string: "https://locate.be-md.ncbi.nlm.nih.gov/")!
 
     var locationURL: URL {
         switch systemType {
         case .development:
-            return SRAA.devURL
+            return Self.devURL
         case .production:
-            return SRAA.prdURL
+            return Self.prdURL
         case .regional:
-            return SRAA.rgnURL
+            return Self.rgnURL
         }
     }
-    private func makeAuth(_ passport: String) -> Data {
-        "{ \"ga4gh_passort\" = \"\(passport)\"}".data(using: .utf8)!
-    }
-    private func makeRequest(_ url: String, _ passport: String?) -> URLRequest {
-        var req = URLRequest(url: URL(string: url, relativeTo: locationURL)!)
-        if let passport = passport {
-            req.httpMethod = "POST"
-            req.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            req.httpBody = makeAuth(passport)
+}
+
+private extension SRAA {
+    /// The configuration used by the shared session
+    private static let sessionConfig: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = TimeInterval(300)
+        config.timeoutIntervalForResource = TimeInterval(300)
+        config.urlCache = URLCache.shared
+        return config
+    }()
+    /// The shared session
+    static let session = URLSession(configuration: sessionConfig)
+}
+
+extension SRAA {
+    private func makeRequest(_ url: String, _ expand: Bool, _ passport: String?) -> URLRequest {
+        var components = URLComponents(string: url)!
+        if expand {
+            if let qi = components.queryItems {
+                components.queryItems = qi + [URLQueryItem(name: "expand", value: "true")]
+            }
+            else {
+                components.queryItems = [URLQueryItem(name: "expand", value: "true")]
+            }
         }
+        let url = components.url(relativeTo: locationURL)!
+        guard let passport = passport else {
+            return URLRequest(url: url)
+        }
+        var req = URLRequest(url: url,
+                             cachePolicy: .reloadIgnoringLocalAndRemoteCacheData)
+        req.httpMethod = "POST"
+        req.allHTTPHeaderFields = ["Content-Type": "application/json"]
+        req.httpBody = try! JSONEncoder().encode(["ga4gh_passport": passport])
         return req
     }
-    func get(url: String, auth: String? = nil) throws -> (HTTPURLResponse, Data) {
+    func get(url: String, expand: Bool = false, auth: String? = nil) throws -> (HTTPURLResponse, Data) {
         var data: Data?
         var response: HTTPURLResponse?
         var error: Error?
+
+        // TODO: Redo for async
         let sema = DispatchSemaphore(value: 0)
-        let task = URLSession.shared.dataTask(with: makeRequest(url, auth)) {
+        let task = SRAA.session.dataTask(with: makeRequest(url, expand, auth)) {
             error = $2
             response = $1 as? HTTPURLResponse
             data = $0
@@ -79,6 +112,7 @@ extension SRAA {
         }
         task.resume()
         _ = sema.wait(timeout: .distantFuture)
+
         if let error = error { throw error }
         return (response!, data!)
     }
